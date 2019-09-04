9980c3787763dfbbece9bac341316e58789a1c4d
espstan <4stanho@gmail.ru>
2019-09-04 12:09:33 +0300

    Register models in travelers/admin.py

--- travelers_project/travelers/admin.py
+++ travelers_project/travelers/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+from .models import PostPhoto
+from .models import Route
+from .models import Sourse
+from .models import Tag
+from .models import Travel
+from .models import Traveler
+
+admin.site.register(Travel)
+admin.site.register(Traveler)
+admin.site.register(Route)
+admin.site.register(Sourse)
+admin.site.register(Post)
+admin.site.register(PostPhoto)
+admin.site.register(Tag)
