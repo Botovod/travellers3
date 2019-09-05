8359a50e20a8f430b32f87c570d203bde11fd4ef
MGDas <mgdas051@gmail.com>
2019-09-05 09:42:12 +0300

    Add url for type-sights

--- travelers_project/geography/urls.py
+++ travelers_project/geography/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import regions, region_detail, city_detail
+from .views import regions, region_detail, city_detail, type_sights
 
 urlpatterns = [
     path('', regions, name='regions_url'),
     path('region/<int:id>/', region_detail, name='region_detail_url'),
     path('city/<int:id>/', city_detail, name='city_detail_url'),
+    path('type-sights/', type_sights, name='type_sights_url'),
 ]
