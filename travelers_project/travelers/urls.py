7b9eb78507b23e083654e138b70e04c263de55ae
espstan <4stanho@gmail.ru>
2019-09-05 14:00:57 +0300

    Edit urls in travelers app

--- travelers_project/travelers/urls.py
+++ travelers_project/travelers/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import TravelerDetail
+from .views import TravelerList
 
 urlpatterns = [
     # ex: /travelers/
-    path('', index, name='index'),
-    # # ex: /polls/5/
-    # path('<int:question_id>/', detail, name='detail'),
-    # # ex: /polls/5/results/
-    # path('<int:question_id>/results/', results, name='results'),
-    # # ex: /polls/5/vote/
-    # path('<int:question_id>/vote/', vote, name='vote'),
+    path('', TravelerList.as_view()),
+    # ex: /travelers/5/
+    path('<int:pk>/', TravelerDetail.as_view()),
+
 ]
