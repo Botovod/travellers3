f6f3c909258b9d01098e3cbdbdc67e3cfa6fdf96
MGDas <mgdas051@gmail.com>
2019-09-05 09:28:21 +0300

    Created view for city page

--- travelers_project/geography/views.py
+++ travelers_project/geography/views.py
@@ -12,3 +12,9 @@ def region_detail(request, id):
     region = get_object_or_404(Region, id=id)
     context = {'region': region}
     return render(request, 'geography/region_detail.html', context)
+
+
+def city_detail(request, id):
+    city = get_object_or_404(City, id=id)
+    context = {'city': city}
+    return render(request, 'geography/city_detail.html', context)
