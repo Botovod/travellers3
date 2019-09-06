0efedf457a57bfc56d19a3d27104cfe54c2bf83b
MGDas <mgdas051@gmail.com>
2019-09-06 10:20:22 +0300

    Deleted not needed views. Created view for sight. Update view for city.

--- travelers_project/geography/views.py
+++ travelers_project/geography/views.py
@@ -1,38 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import City, Sight, Region, TypeOfSights
+from geography.models import City, Sight, Region, TypeOfSights
 
 
 class RegionList(ListView):
     model = Region
     template_name = 'geography/regions.html'
     context_object_name = 'region_list'
-region_list = RegionList.as_view()
 
 
 class RegionDetail(DetailView):
     model = Region
     template_name = 'geography/region_detail.html'
     context_object_name = 'region_detail'
-region_detail = RegionDetail.as_view()
+
+
+class CityList(ListView):
+    model = City
+    template_name = 'geography/city_list.html'
+    context_object_name = 'city_list'
 
 
 class CityDetail(DetailView):
     model = City
     template_name = 'geography/city_detail.html'
     context_object_name = 'city_detail'
-city_detail = CityDetail.as_view()
 
 
-class TypeOfSightsList(ListView):
-    model = TypeOfSights
-    template_name = 'geography/type_sights.html'
-    context_object_name = 'type_sights'
-type_sights = TypeOfSightsList.as_view()
+class SightList(ListView):
+    model = Sight
+    template_name = 'geography/sight_list.html'
+    context_object_name = 'sight_list'
 
 
-class TypeOfSightsDetail(DetailView):
-    model = TypeOfSights
-    template_name = 'geography/type_sight_detail.html'
-    context_object_name = 'type_sight_detail'
-type_sight_detail = TypeOfSightsDetail.as_view()
+class SightDetail(DetailView):
+    model = Sight
+    template_name = 'geography'
+    context_object_name = 'sight_detail'
