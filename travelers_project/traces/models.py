4702bd80433b5a9b9124c417905f7a87b8916f80
espstan <4stanho@gmail.ru>
2019-09-06 11:58:03 +0300

    Add models in app traces

--- travelers_project/traces/models.py
+++ travelers_project/traces/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from geography.models import City
+from geography.models import Sight
+
+
+class RouteByCities(models.Model):
+    title = models.CharField(max_length=255, default='', verbose_name='Название маршрута')
+    cities = models.ManyToManyField(City, blank=False, verbose_name='Города маршрута')
+
+    class Meta:
+        verbose_name = 'Маршрут по городам'
+        verbose_name_plural = 'Маршруты по городам'
+
+    def __str__(self):
+        return self.title
+
+
+class RouteBySights(models.Model):
+    title = models.CharField(max_length=255, default='', verbose_name='Название маршрута')
+    sights = models.ManyToManyField(Sight, blank=False, verbose_name='Достопримечательности маршрута')
+
+    class Meta:
+        verbose_name = 'Маршрут по достопримечательностям'
+        verbose_name_plural = 'Маршруты по достопримечательностям'
+
+    def __str__(self):
+        return self.title
