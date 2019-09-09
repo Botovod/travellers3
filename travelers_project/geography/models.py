d63b7783a0caaaa200379535fdea7cf79dc09594
MGDas <mgdas051@gmail.com>
2019-09-09 15:57:08 +0300

    fixed models field

--- travelers_project/geography/models.py
+++ travelers_project/geography/models.py
@@ -92,8 +92,8 @@ class SectionOfSights(BaseModel):
 
 
 class SightPhoto(models.Model):
-    sight = models.ForeignKey(Sight, on_delete=models.CASCADE, related_name='sigth_photo', null=True, verbose_name='Достпримечательность')
-    file = models.ImageField(default='', upload_to='photo/photo/', verbose_name='Изображение')
+    sight = models.ForeignKey(Sight, on_delete=models.CASCADE, related_name='sight_photo', null=True, verbose_name='Достпримечательность')
+    file = models.ImageField(default='', upload_to='photo/', verbose_name='Изображение')
     posted = models.BooleanField('Опубликовано', default=True)
 
     class Meta:
