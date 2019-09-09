f14b4d56851d03323c7b1c00f79df11bf6d83c77
MGDas <mgdas051@gmail.com>
2019-09-09 16:07:58 +0300

    Deleted functions, models, views, url

--- travelers_project/travelers/urls.py
+++ travelers_project/travelers/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
 
 from .views import BaseTemplate
-from .views import TravelerDetail
-from .views import TravelerList
 
 urlpatterns = [
     # ex: /
     path('', BaseTemplate.as_view(), name='base_url'),
-    # ex: /travelers/
-    path('travelers/', TravelerList.as_view()),
-    # ex: /travelers/5/
-    path('travelers/<int:pk>/', TravelerDetail.as_view()),
-
 ]
--- travelers_project/travelers/views.py
+++ travelers_project/travelers/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from django.views.generic import TemplateView
 
 from geography.models import Sight
-from travelers.models import Traveler
+
 
 class BaseTemplate(TemplateView):
     template_name = 'base.html'
@@ -12,15 +12,3 @@ class BaseTemplate(TemplateView):
         context = super().get_context_data(**kwrags)
         context['sights'] = Sight.objects.all()
         return context
-
-
-class TravelerList(ListView):
-    model = Traveler
-    context_object_name = 'traveler_list'
-    template_name = 'travelers/index.html'
-
-
-class TravelerDetail(DetailView):
-    model = Traveler
-    template_name = 'travelers/traveler_detail.html'
-    context_object_name = 'traveler_detail'
