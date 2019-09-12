04b4f0a710080976bf77853eb2121d1354ecce48
espstan <4stanho@gmail.ru>
2019-09-12 10:33:29 +0300

    Добавлены пути api TypeOfSights

--- travelers_project/geography/api/urls.py
+++ travelers_project/geography/api/urls.py
@@ -6,6 +6,8 @@ from geography.api.views import CityList
 from geography.api.views import CityDetail
 from geography.api.views import SightList
 from geography.api.views import SightDetail
+from geography.api.views import TypeOfSightsList
+from geography.api.views import TypeOfSightsDetail
 
 urlpatterns = [
     path('regions/', RegionList.as_view()),
@@ -14,5 +16,7 @@ urlpatterns = [
     path('city/<int:pk>/', CityDetail.as_view()),
     path('sights/', SightList.as_view()),
     path('sight/<int:pk>/', SightDetail.as_view()),
+    path('typeofsights/', TypeOfSightsList.as_view()),
+    path('typeofsights/<int:pk>/', TypeOfSightsDetail.as_view()),
 
 ]
