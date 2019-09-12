bb987387c5f70afc0ad2d2a235a1bd30b2329be5
espstan <4stanho@gmail.ru>
2019-09-12 10:14:06 +0300

    Добавлены контроллеры api Sight

--- travelers_project/geography/api/views.py
+++ travelers_project/geography/api/views.py
@@ -2,11 +2,14 @@ from rest_framework import generics
 
 from geography.models import City
 from geography.models import Region
+from geography.models import Sight
 
 from geography.api.serializers import RegionListSerializer
 from geography.api.serializers import RegionDetailSerializer
 from geography.api.serializers import CityListSerializer
 from geography.api.serializers import CityDetailSerializer
+from geography.api.serializers import SightListSerializer
+from geography.api.serializers import SightDetailSerializer
 
 
 class RegionList(generics.ListCreateAPIView):
@@ -27,3 +30,13 @@ class CityList(generics.ListCreateAPIView):
 class CityDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = City.objects.all()
     serializer_class = CityDetailSerializer
+
+
+class SightList(generics.ListCreateAPIView):
+    queryset = Sight.objects.order_by('id')
+    serializer_class = SightListSerializer
+
+
+class SightDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sight.objects.all()
+    serializer_class = SightDetailSerializer
