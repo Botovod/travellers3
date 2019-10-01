3458301e043a46f2082a04ba781a1716c4280be3
espstan <4stanho@gmail.ru>
2019-10-01 12:05:52 +0300

    ISSUE-83 Deleting media/

--- travelers_project/travelers_project/wsgi.py
+++ travelers_project/travelers_project/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import urls
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelers_project.settings')
 
