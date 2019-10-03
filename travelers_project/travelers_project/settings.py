7b403c7c6217cbbd44ca0f3c69fdfdcdcf056a6f
espstan <4stanho@gmail.ru>
2019-10-03 18:42:42 +0300

    ISSUE-88 Edit static and templates folders

--- travelers_project/travelers_project/settings.py
+++ travelers_project/travelers_project/settings.py
@@ -49,7 +49,9 @@ ROOT_URLCONF = 'travelers_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'travelers/templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
