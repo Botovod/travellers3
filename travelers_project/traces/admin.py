8dba11986645d2d0aa6bc90831524932a67ccdbf
espstan <4stanho@gmail.ru>
2019-10-07 18:26:33 +0300

    Add CitiesRelationship

--- travelers_project/traces/admin.py
+++ travelers_project/traces/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from traces.models import RouteByCities
+from traces.models import RouteByCities, CitiesRelationship
 from traces.models import RouteBySights
 
 admin.site.register(RouteByCities)
 admin.site.register(RouteBySights)
+admin.site.register(CitiesRelationship)
+
