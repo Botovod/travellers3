c94517202b96d37d62a7e11bf4d1e37e5738e054
MGDas <mgdas051@gmail.com>
2019-10-09 14:50:29 +0300

    Fixed

--- travelers_project/autotraveler_parser/connect.py
+++ travelers_project/autotraveler_parser/connect.py
@@ -24,11 +24,15 @@ async def connect(url):
 async def connect_for_images(url):
     base_url = 'http://autotravel.ru'
 
-    async with aiohttp.ClientSession() as session:
-        while True:
-            async with session.get(base_url + url, ssl=False) as response:
-                if response.status != 503:
-                    data = await response.read()
-                    return data
-                else:
-                    await asyncio.sleep(2)
+    while True:
+        try:
+            async with aiohttp.ClientSession() as session:
+                while True:
+                    async with session.get(base_url + url, ssl=False) as response:
+                        if response.status != 503:
+                            data = await response.read()
+                            return data
+                        else:
+                            await asyncio.sleep(2)
+        except Exception as e:
+            continue
