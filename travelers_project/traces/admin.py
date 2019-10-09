a06785ffa66792a2d1bdda05346d5ec0c7edd3ae
espstan <4stanho@gmail.ru>
2019-10-09 09:38:44 +0300

    ISSUE-74 Add SightsRelationshipAdmin in traces admin

--- travelers_project/traces/admin.py
+++ travelers_project/traces/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
 from traces.models import RouteByCities, CitiesRelationship
-from traces.models import RouteBySights
+from traces.models import RouteBySights, SightsRelationship
+
+
+class SightsRelationshipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sight', 'route', 'position')
+    list_display_links = ('sight', 'route', 'position')
+    search_fields = ['id', 'sight']
+
 
 admin.site.register(RouteByCities)
 admin.site.register(RouteBySights)
 admin.site.register(CitiesRelationship)
+admin.site.register(SightsRelationship, SightsRelationshipAdmin)
 
