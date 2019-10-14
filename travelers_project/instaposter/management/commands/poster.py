1c28013466363fdb387c164837725e953af7075e
MGDas <mgdas051@gmail.com>
2019-10-14 11:46:10 +0300

    Created poster for instagram

---
+++ travelers_project/instaposter/management/commands/poster.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+from instaposter.models import Post
+from django.conf import settings
+from instapy_cli import client
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+
+        username = settings.INST_USERNAME
+        password = settings.INST_PASSWORD
+        cookie_file = os.path.join(settings.BASE_DIR, 'travelers_project/user_ig.json')
+
+        try:
+            post = Post.objects.first()
+        except ObjectDoesNotExist as e:
+            post = ''
+            logger.error(e)
+
+        if post:
+            image = os.path.join(settings.MEDIA_ROOT, str(post.image))
+            text = post.comment
+
+            with client(username, password, cookie_file=cookie_file, write_cookie_file=True) as cli:
+                cli.upload(image, text)
+                print(f"Post {text[:10]}... Done!!!")
