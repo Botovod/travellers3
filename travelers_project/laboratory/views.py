5a967b0830246192cef14a318f0e7d46d9b76ef2
MGDas <mgdas051@gmail.com>
2019-11-13 21:27:35 +0300

    Added trace's cities

--- travelers_project/laboratory/views.py
+++ travelers_project/laboratory/views.py
@@ -36,10 +36,10 @@ def get_data():
         for region_id, title in regions:
             dictionary = dict()
             dictionary['region'] = title
-            cur.execute('''SELECT id, title 
+            cur.execute('''SELECT id, title
                            FROM geography_city
-                           WHERE region_id = {} 
-                           AND rating = 
+                           WHERE region_id = {}
+                           AND rating =
                            (SELECT Max(rating)
                            FROM geography_city
                            WHERE region_id = {}) ;'''.format(region_id, region_id))
@@ -61,7 +61,26 @@ class TopTracesListView(ListView):
     def get(self, request):
         with ConnPsql() as conn:
             cursor = conn.cursor()
-            cursor.execute('''SELECT id, title FROM traces_routebycities;''')
-            traces = cursor.fetchall()
 
-        return render(request, self.template_name, {'traces': traces})
+            cursor.execute(
+                '''
+                    SELECT id, title FROM traces_routebycities;
+                '''
+            )
+            traces = cursor.fetchall()      # [(1, 'Москва - Санкт-Петербург')]
+
+            routes = {}                     # {(1, 'Москва - Санкт-Петербург'): [[(1, Moscow), (2, Sbp), ...], Kremle, image_url}
+            for trace in traces:
+                cursor.execute(
+                    f'''
+                        SELECT geography_city.id, title
+                        FROM geography_city
+                        INNER JOIN traces_citiesrelationship
+                        ON (geography_city.id = traces_citiesrelationship.city_id)
+                        WHERE traces_citiesrelationship.route_id = {trace[0]}
+                    '''
+                )
+                cities = cursor.fetchall()
+                routes[trace] = cities
+
+        return render(request, self.template_name, {'routes': routes})
