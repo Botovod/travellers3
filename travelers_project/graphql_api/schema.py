0b9bf8711b8a78e022331e76ef7624ef84cb0c39
espstan <4stanho@gmail.com>
2019-11-19 18:37:18 +0300

    ISSUE-165 Add query for best cities in selected region

--- travelers_project/graphql_api/schema.py
+++ travelers_project/graphql_api/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+from django.db.models import Max
 
 from geography.models import City, Region
 
@@ -16,7 +17,10 @@ class RegionType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_cities = graphene.List(CityType)
+    all_regions = graphene.List(RegionType)
     region = graphene.Field(RegionType, title=graphene.String())
+    best_cities = graphene.List(CityType, id=graphene.Int())
+
 
     def resolve_all_cities(self, info, **kwargs):
         return City.objects.all()
@@ -27,3 +31,16 @@ class Query(graphene.ObjectType):
         if title:
             return Region.objects.get(title=title)
         return None
+
+    def resolve_best_cities(self, info, **kwargs):
+        region_id = kwargs.get('id')
+        cities = City.objects.filter(region=region_id)
+        city_max = cities.aggregate(Max('rating'))
+        filtered_cities = cities.filter(rating=city_max['rating__max'])
+
+        if region_id:
+            return filtered_cities
+        return None
+
+    def resolve_all_regions(self, info, **kwargs):
+        return Region.objects.all()
