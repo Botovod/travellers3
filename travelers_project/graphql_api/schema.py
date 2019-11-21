abee7e099fcc225634f7e524a33d085734e528e5
espstan <espstan@gmail.com>
2019-11-21 10:40:43 +0300

    Add query for best sights in current region

--- travelers_project/graphql_api/schema.py
+++ travelers_project/graphql_api/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from django.db.models import Max
 
-from geography.models import City, Region
+from geography.models import City, Region, Sight
 
 
 class CityType(DjangoObjectType):
@@ -15,11 +15,17 @@ class RegionType(DjangoObjectType):
         model = Region
 
 
+class SightType(DjangoObjectType):
+    class Meta:
+        model = Sight
+
+
 class Query(graphene.ObjectType):
     all_cities = graphene.List(CityType)
     all_regions = graphene.List(RegionType)
     region = graphene.Field(RegionType, title=graphene.String())
     best_cities = graphene.List(CityType, id=graphene.Int())
+    best_sight_in_region = graphene.List(SightType, id=graphene.Int())
 
 
     def resolve_all_cities(self, info, **kwargs):
@@ -44,3 +50,13 @@ class Query(graphene.ObjectType):
 
     def resolve_all_regions(self, info, **kwargs):
         return Region.objects.all()
+
+    def resolve_best_sight_in_region(self, info, **kwargs):
+        region_id = kwargs.get('id')
+        sights = Sight.objects.filter(city__in=City.objects.filter(region=region_id))
+        sight_max = sights.aggregate(Max('rating'))
+        filtered_sights = sights.filter(rating=sight_max['rating__max'])
+
+        if region_id:
+            return filtered_sights
+        return None
