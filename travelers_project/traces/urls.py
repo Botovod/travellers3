76bc81aa871cacd17b47a90a23da7c6a600f7485
espstan <espstan@gmail.com>
2019-12-09 18:08:28 +0300

    ISSUE-195 Use urls with router

--- travelers_project/traces/urls.py
+++ travelers_project/traces/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import SimpleRouter
 
 from traces.views_list.views import RouteByCitiesDetail
 from traces.views_list.views import RouteByCitiesList
@@ -13,21 +13,17 @@ from traces.views_list.view_sets import (
     SightsRelationshipViewSet,
 )
 
-router = routers.DefaultRouter()
-router.register('router_by_cities', RouteByCitiesViewSet, basename='router_by_city')
-router.register('cities_relations', CitiesRelationshipViewSet, basename='city_relation')
-router.register('router_by_sights', RouteBySightsViewSet, basename='router_by_sight')
-router.register('sights_relations', SightsRelationshipViewSet, basename='sight_relation')
-
 urlpatterns = [
-    # ex: traces/route_cities
     path('route-cities/', RouteByCitiesList.as_view(), name='routes-cities-list-url'),
-    # ex: traces/route_cities/1/
     path('route-cities/<int:pk>/', RouteByCitiesDetail.as_view(), name='routes-cities-detail-url'),
-    # ex: traces/route_sigths
     path('route-sights/', RouteBySightsList.as_view(), name='routes-sights-list-url'),
-    # ex: traces/route_sights/1/
     path('route-sights/<int:pk>/', RouteBySightsDetail.as_view(), name='routes-sights-detail-url'),
-    # ex: api/v1/
-    path('api/v1/', include(router.urls)),
 ]
+
+router = SimpleRouter()
+router.register('api/v1/router_by_cities', RouteByCitiesViewSet, basename='router_by_city')
+router.register('api/v1/cities_relations', CitiesRelationshipViewSet, basename='city_relation')
+router.register('api/v1/router_by_sights', RouteBySightsViewSet, basename='router_by_sight')
+router.register('api/v1/sights_relations', SightsRelationshipViewSet, basename='sight_relation')
+
+urlpatterns.extend(router.urls)
