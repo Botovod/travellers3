be9f9a1734ed7d1991d52f331c62b26769a02370
KostyaT <kotofey_98@mail.ru>
2020-01-22 12:57:18 +0300

    added model of the mainpage

--- travelers_project/mainpage/models.py
+++ travelers_project/mainpage/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class MainPage(models.Model):
+    main_banner = models.ImageField()
+    big_description = models.CharField(max_length=255, default='', verbose_name='Большое описание')
+    small_description = models.CharField(max_length=255, default='', verbose_name='Малое описание')
+    link = models.CharField(max_length=255, default='', verbose_name='Ссылка')
+    left_small_tile_title = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Заголовок маленькой левой плитки')
+    left_small_tile_description = models.TextField(
+        default='',
+        blank=True,
+        verbose_name='Описание маленькой левой плитки')
+    center_small_tile_title = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Заголовок маленькой центральной плитки')
+    center_small_tile_description = models.TextField(
+        default='',
+        blank=True,
+        verbose_name='Описание маленькой центральной плитки')
+    right_small_tile_title = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Заголовок маленькой правой плитки')
+    right_small_tile_description = models.TextField(
+        default='',
+        blank=True,
+        verbose_name='Описание маленькой правой плитки')
+    video_link = models.CharField(max_length=255, default='', verbose_name='Видео')
+
+    left_big_tile_header = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Большой заголовок большой левой плитки')
+    left_big_tile_title = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Заголовок большой левой плитки')
+    left_big_tile_description = models.TextField(
+        default='',
+        blank=True,
+        verbose_name='Описание большой левой плитки')
+    right_big_tile_header = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Большой заголовок большой правой плитки')
+    right_big_tile_title = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='Заголовок большой правой плитки')
+    right_big_tile_description = models.TextField(
+        default='',
+        blank=True,
+        verbose_name='Описание большой правой плитки')
