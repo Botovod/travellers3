a7753325ba066be1b52a7c4ede34f4d76069d50b
KostyaT <kotofey_98@mail.ru>
2020-01-23 10:21:05 +0300

    models added

--- travelers_project/trips/models.py
+++ travelers_project/trips/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Traveler(models.Model):
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Trip(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название путешествия')
+    start_date = models.DateField(verbose_name='Дата начала путешествия')
+    end_date = models.DateField(verbose_name='Дата окончания путешествия')
+    description = models.TextField(default='', verbose_name='Описание путешествия')
+    traveler = models.ManyToManyField(Traveler, related_name='trip_traveler', verbose_name='Путешественник')
+
+    def __str__(self):
+        return self.title
