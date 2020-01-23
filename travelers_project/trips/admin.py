dba9ce78514b7c0fe5a9938fea4bb5084c1e08d3
KostyaT <kotofey_98@mail.ru>
2020-01-23 13:06:20 +0300

    fixed models

--- travelers_project/trips/admin.py
+++ travelers_project/trips/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Traveler, Trip
 
-# Register your models here.
+admin.site.register(Traveler)
+admin.site.register(Trip)
--- travelers_project/trips/models.py
+++ travelers_project/trips/models.py
@@ -1,33 +1,36 @@
 from django.db import models
 from traces.models import RouteByCities, RouteBySights
+from django.contrib.auth.models import User
 
 
 class Traveler(models.Model):
-    first_name = models.CharField(max_length=255, verbose_name='Имя')
-    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    birth_date = models.DateTimeField(verbose_name='Дата рождения')
 
     class Meta:
         verbose_name = 'Путешественник'
         verbose_name_plural = 'Путешественники'
-        ordering = ['last_name']
-
-    def __str__(self):
-        return f'{self.last_name} {self.first_name}'
 
 
 class Trip(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название путешествия')
-    start_date = models.DateField(verbose_name='Дата начала путешествия')
-    end_date = models.DateField(verbose_name='Дата окончания путешествия')
+    start_date = models.DateTimeField(verbose_name='Дата начала путешествия')
+    end_date = models.DateTimeField(verbose_name='Дата окончания путешествия')
     description = models.TextField(default='', verbose_name='Описание путешествия')
     traveler = models.ManyToManyField(Traveler, related_name='trip_traveler', verbose_name='Путешественники')
-    route_by_sights = models.ManyToManyField(
+    route_by_sights = models.ForeignKey(
         RouteBySights,
+        on_delete=models.DO_NOTHING,
         related_name='trip_route_sight',
+        blank=True,
+        null=True,
         verbose_name='Путешествие по маршрутам достопримечательностей')
-    route_by_cities = models.ManyToManyField(
+    route_by_cities = models.ForeignKey(
         RouteByCities,
+        on_delete=models.DO_NOTHING,
         related_name='trip_route_city',
+        blank=True,
+        null=True,
         verbose_name='Путешествие по маршрутам городов')
 
     class Meta:
