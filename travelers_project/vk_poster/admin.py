5b347f58508758aac1a0695d043dcd5860b36a48
truefan4ever <kotofey_98@mail.ru>
2020-03-09 12:51:33 +0300

    app for vk posts was added

---
+++ travelers_project/vk_poster/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import CityPost, SightPost, CityTripPost
+
+admin.site.register(CityPost)
+admin.site.register(SightPost)
+admin.site.register(CityTripPost)
